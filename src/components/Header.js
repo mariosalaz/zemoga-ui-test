3cfd17638db2316b0645b5e3c8a1ed4d90fd4097
Mario Salazar <jorge.salazar@globant.com>
2020-01-11 12:56:23 -0500

    main folders created

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+
 
 const Header = props => {
     return (
-        <div>
-            Hello world
-        </div>
+        <header>
+            <nav>
+                <div className="header__title">
+                    <h2>Rule of Thumb.</h2>
+                </div>
+                <div className="header__nav-container">
+                    <ul className="header__nav-items">
+                        <li className="header__nav-item">past trials</li>
+                        <li className="header__nav-item">how it works</li>
+                        <li className="header__nav-item">log in / sign up</li>
+                        <span>
+                            <FontAwesomeIcon icon ={faSearch}/>
+                        </span>
+                    </ul>
+                </div>
+            </nav>
+        </header>
     )
 }
 
